8fe39b4f231ea59d1a7732fcc42aed4dd5fa3697
Nicola Selleri <nicola.selleri@fao.org>
2015-11-27 20:47:46 +0100

    home-intro

--- js/home-intro.js
+++ js/home-intro.js
@@ -1,50 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>three.js webgl - collada</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <style>
+$( document ).ready(function() {
 
-        body {
-            font-family: Monospace;
-            background-color: #000000;
-            margin: 0px;
-            overflow: hidden;
-        }
-
-        #info {
-            color: #fff;
-            position: absolute;
-            top: 10px;
-            width: 100%;
-            text-align: center;
-            z-index: 100;
-            display: block;
-
-        }
-
-        a {
-            color: skyblue
-        }
-
-    </style>
-</head>
-<body>
-
-<div id="test"></div>
-
-
-<script src="js/three.min.js"></script>
-<script src="js/ColladaLoader.js"></script>
-<script src="js/Detector.js"></script>
-<script src="js/stats.min.js"></script>
-<script src="js/jquery-1.11.3.min.js"></script>
-<script src="js/inertiaScroll.js"></script>
-
-
-<script>
+    //$('.home-container').toggleClass('visible');
 
     if (!Detector.webgl) Detector.addGetWebGLMessage();
 
@@ -66,7 +22,7 @@
     var chiesaAlpha;
     initialized = false;
 
-    var modelPath = 'ultimo/venezia_03.dae';
+    var modelPath = 'ultimo/venezia_specchio.dae';
 
 
     function init() {
@@ -76,10 +32,10 @@
         var container = document.createElement('div');
         document.body.appendChild(container);
         container.id = 'wheel';
-        $( "#wheel" ).addClass( "sixteen-nine" );
+        //$( "#wheel" ).addClass( "sixteen-nine" );
         // Camera
-//        model.getObjectByName( 'Camera', true ).visible = true;
-//        console.log();
+    //        model.getObjectByName( 'Camera', true ).visible = true;
+    //        console.log();
 
 
         //camera = new THREE.PerspectiveCamera(36, window.innerWidth / window.innerHeight, 0.01, 100000);
@@ -105,65 +61,65 @@
 
         // Grid
 
-//        var material = new THREE.LineBasicMaterial( { color: 0x303030 } );
-//        var geometry = new THREE.Geometry();
-//        var floor = -0.04, step = 1, size = 14;
-//
-//        for ( var i = 0; i <= size / step * 2; i ++ ) {
-//
-//            geometry.vertices.push( new THREE.Vector3( - size, floor, i * step - size ) );
-//            geometry.vertices.push( new THREE.Vector3(   size, floor, i * step - size ) );
-//            geometry.vertices.push( new THREE.Vector3( i * step - size, floor, -size ) );
-//            geometry.vertices.push( new THREE.Vector3( i * step - size, floor,  size ) );
-//
-//        }
-//
-//        var line = new THREE.LineSegments( geometry, material );
-//        scene.add( line );
+    //        var material = new THREE.LineBasicMaterial( { color: 0x303030 } );
+    //        var geometry = new THREE.Geometry();
+    //        var floor = -0.04, step = 1, size = 14;
+    //
+    //        for ( var i = 0; i <= size / step * 2; i ++ ) {
+    //
+    //            geometry.vertices.push( new THREE.Vector3( - size, floor, i * step - size ) );
+    //            geometry.vertices.push( new THREE.Vector3(   size, floor, i * step - size ) );
+    //            geometry.vertices.push( new THREE.Vector3( i * step - size, floor, -size ) );
+    //            geometry.vertices.push( new THREE.Vector3( i * step - size, floor,  size ) );
+    //
+    //        }
+    //
+    //        var line = new THREE.LineSegments( geometry, material );
+    //        scene.add( line );
 
         // Add the COLLADA
 
 
-//        model.getObjectByName( 'pasted__pCube1', true ).visible = false;
+    //        model.getObjectByName( 'pasted__pCube1', true ).visible = false;
 
 
-//        var sferaGeometria = new THREE.SphereGeometry(100, 30, 30); // Radius, number of width segment, number of height segment Only radius is required
-//        var terraMateriale = new THREE.MeshPhongMaterial(); // Reacts to lights
-//
-//        terraMateriale.map = cerchioAlpha;
-//        terraMateriale.alphaMap = cerchioAlpha;
-//        terraMateriale.transparent = true
-//
-//        var terraMesh = new THREE.Mesh(sferaGeometria, terraMateriale);
-//
-//        scene.add( terraMesh );
+    //        var sferaGeometria = new THREE.SphereGeometry(100, 30, 30); // Radius, number of width segment, number of height segment Only radius is required
+    //        var terraMateriale = new THREE.MeshPhongMaterial(); // Reacts to lights
+    //
+    //        terraMateriale.map = cerchioAlpha;
+    //        terraMateriale.alphaMap = cerchioAlpha;
+    //        terraMateriale.transparent = true
+    //
+    //        var terraMesh = new THREE.Mesh(sferaGeometria, terraMateriale);
+    //
+    //        scene.add( terraMesh );
         scene.add(model);
 
 
         // Lights
 
-//        pointLight = new THREE.PointLight( 0xffffff, 1);
-//        scene.add( pointLight );
+    //        pointLight = new THREE.PointLight( 0xffffff, 1);
+    //        scene.add( pointLight );
 
         var light = new THREE.AmbientLight( 0xffffff ); // soft white light
         scene.add( light );
 
 
-//        var pianoGeometria = new THREE.PlaneGeometry(2000, 2000, 2000);
-//        var pianoMateriale = new THREE.MeshLambertMaterial({
-//            color: 0xff0000
-//
-//        });
-//
-//
-//
-//        var piano = new THREE.Mesh(pianoGeometria, pianoMateriale);
-//        piano.receiveShadow = true; //Shadow, both casted and received, have to be enabled
-//        piano.rotation.x = -0.5 * Math.PI; //90 degrees in radians
-//        piano.position.set(0, -2, 0);
-//
-//
-//        scene.add(piano);
+    //        var pianoGeometria = new THREE.PlaneGeometry(2000, 2000, 2000);
+    //        var pianoMateriale = new THREE.MeshLambertMaterial({
+    //            color: 0xff0000
+    //
+    //        });
+    //
+    //
+    //
+    //        var piano = new THREE.Mesh(pianoGeometria, pianoMateriale);
+    //        piano.receiveShadow = true; //Shadow, both casted and received, have to be enabled
+    //        piano.rotation.x = -0.5 * Math.PI; //90 degrees in radians
+    //        piano.position.set(0, -2, 0);
+    //
+    //
+    //        scene.add(piano);
 
         // Renderer
 
@@ -251,41 +207,45 @@
 
 
         //var frameTime = ( timestamp - lastTimestamp ) * 0.001;
-//
-//        if ( progress >= 0 && progress < kfAnimationsLength ) {
-//
-//            for ( var i = 0; i < kfAnimationsLength; ++i ) {
-//
-//                kfAnimations[ i ].update( frameTime );
-//
-//            }
-//
-//        } else if ( progress >= kfAnimationsLength ) {
-//
-//            for ( var i = 0; i < kfAnimationsLength; ++i ) {
-//
-//                kfAnimations[ i ].stop();
-//
-//            }
-//
-//            progress = 0;
-//            start();
-//
-//        }
+    //
+    //        if ( progress >= 0 && progress < kfAnimationsLength ) {
+    //
+    //            for ( var i = 0; i < kfAnimationsLength; ++i ) {
+    //
+    //                kfAnimations[ i ].update( frameTime );
+    //
+    //            }
+    //
+    //        } else if ( progress >= kfAnimationsLength ) {
+    //
+    //            for ( var i = 0; i < kfAnimationsLength; ++i ) {
+    //
+    //                kfAnimations[ i ].stop();
+    //
+    //            }
+    //
+    //            progress = 0;
+    //            start();
+    //
+    //        }
 
         //console.log(scene)
 
         var VIDEO_INIT = 0,
-                VIDEO_END = 65,
-                auxTime;
+            VIDEO_END = 65,
+            auxTime;
 
         auxTime = currentTime + e;
 
         if (auxTime <= VIDEO_END & auxTime >= VIDEO_INIT) {
 
+
+
             currentTime = auxTime;
 
             var cooD = e;
+
+
 
             model.traverse(function (child) {
 
@@ -296,8 +256,8 @@
                     camera = child;
                     camera.setLens (50, 35 );
                     //camera.near = 0;
-//                camera.fov = 30;
-//                camera.castShadow = true;
+    //                camera.fov = 30;
+    //                camera.castShadow = true;
                     var vector = new THREE.Vector3();
                     vector.setFromMatrixPosition(model.matrixWorld);
 
@@ -306,6 +266,12 @@
 
 
             });
+
+
+            var position =  parseInt(currentTime*10, 10);
+
+
+           // updateOverlay(position);
 
             camera.aspect = window.innerWidth / window.innerHeight;
             //camera.setLens ( 36, 35 )
@@ -329,7 +295,7 @@
         //pointLight.position.set(0,200,200);
 
         //progress += frameTime;
-//        lastTimestamp = timestamp;
+    //        lastTimestamp = timestamp;
         renderer.render(scene, camera);
         stats.update();
         startAnimation()
@@ -372,8 +338,8 @@
         var w = evt.wheelDelta, d = evt.detail;
         if (d) {
             if (w) return w / d / 40 * d > 0 ? 1 : -1; // Opera
-            else return -d / 3;              // Firefox;
-        } else return w / 120;             // IE/Safari/Chrome
+            else return -d / 3;              // Firefox;         TODO: do not /3 for OS X
+        } else return w / 120;             // IE/Safari/Chrome TODO: /3 for Chrome OS X
     };
 
     var wheelDirection = function (evt) {
@@ -416,7 +382,7 @@
                 //mouseRatio: 6,
                 onChange: function (coords) {
 
-                    console.log(coords);
+
 
                     stopAnimation();
 
@@ -427,15 +393,23 @@
                     //console.log('progress '+ progress);
                     //console.log('lastTimestamp '+ lastTimestamp);
 
+
+
+
+
+
+
+
+                    //console.log(progress.toFixed(2));
                     //Invert scroll direction
                     update(progress.toFixed(2) * -0.01);
 
                     lastTimestamp = coords.y;
 
-//            $('.wheel > div').each(function(i){
-//                //var angle = -(coords.y/2) + (360/10)*i;
-//                //$(this).css('transform', 'perspective(500px) rotate3d(1,0,0,'+angle+'deg) translate3d(0,0,122px)');
-//            });
+    //            $('.wheel > div').each(function(i){
+    //                //var angle = -(coords.y/2) + (360/10)*i;
+    //                //$(this).css('transform', 'perspective(500px) rotate3d(1,0,0,'+angle+'deg) translate3d(0,0,122px)');
+    //            });
                 }
             });
 
@@ -486,9 +460,11 @@
 
                 function processArray( array ) {
 
-                    console.log('processArray' + array)
+                    //console.log('processArray' + array)
 
-                    var forbiddenNames = ['Light', 'Plane', 'Camera', 'Nullo', 'group1_pPlane8'];
+                    var forbiddenNames = ['Light', 'Plane', 'Camera', 'Nullo', 'group1_pPlane8','mare','Null_Object'];
+
+                    var transparent = ['mare'];
 
                     for (var i = 0; i < array.length; i++) {
 
@@ -498,7 +474,7 @@
 
                             var object = model.getObjectByName(name, true);
 
-                            console.log()
+                            //console.log(object)
 
                             if ( name.indexOf('gruppo') > -1 ) {
 
@@ -510,6 +486,16 @@
                             }
 
                         }
+
+                        if ($.inArray(name, transparent) >= 0) {
+
+                            var object = model.getObjectByName(name, true);
+
+                            console.log(object);
+
+                            applyTransparency(name, object);
+
+                        }
                     }
 
                 }
@@ -519,10 +505,9 @@
                     console.log('loadAlpha' + name)
 
                     var file = 'ultimo/' + name + '_alpha.jpg',
-                            alpha = new THREE.Texture(),
-                            material = object.children[0].material;
+                        alpha = new THREE.Texture(),
+                        material = object.children[0].material;
 
-                    console.log(123456789)
 
                     //object.renderOrder = 1
                     material.alphaMap = alpha;
@@ -536,6 +521,18 @@
                         alpha.needsUpdate = true;
 
                     });
+
+                }
+
+                function applyTransparency(name, object) {
+
+                    console.log('applyTransparency' + name)
+
+                    var material = object.children[0].material;
+
+                    material.transparent = true;
+                    material.opacity= 0.5;
+
 
                 }
 
@@ -555,6 +552,85 @@
 
     Animation.init();
 
-</script>
-</body>
-</html>
+
+    function updateOverlay(e){
+
+        console.log('c:'+ e);
+
+        if(e > 0){
+
+            $('.home-container').addClass('visible');
+        }
+
+        if(e > 2){
+            //console.log('c:'+ c);
+            $('.home-container').removeClass('visible');
+        }
+
+
+        if(e < 40){
+            //console.log('c:'+ c);
+            $('.message-1').removeClass('visible');
+        }
+
+
+        if(e > 40){
+            //console.log('c:'+ c);
+            $('.message-1').addClass('visible');
+        }
+
+        if(e > 60){
+            //console.log('c:'+ c);
+            $('.message-1').removeClass('visible');
+        }
+
+        if(e < 235){
+            //console.log('c:'+ c);
+            $('.message-2').removeClass('visible');
+        }
+
+        if(e > 235){
+            //console.log('c:'+ c);
+            $('.message-2').addClass('visible');
+        }
+
+        if(e > 255){
+            //console.log('c:'+ c);
+            $('.message-2').removeClass('visible');
+        }
+
+        if(e < 487){
+            //console.log('c:'+ c);
+            $('.message-3').removeClass('visible');
+        }
+
+        if(e > 487){
+            //console.log('c:'+ c);
+            $('.message-3').addClass('visible');
+        }
+
+        if(e > 505){
+            //console.log('c:'+ c);
+            $('.message-3').removeClass('visible');
+        }
+
+        if(e < 626){
+            //console.log('c:'+ c);
+            $('.message-4').removeClass('visible');
+        }
+
+        if(e > 626){
+            //console.log('c:'+ c);
+            $('.message-4').addClass('visible');
+        }
+
+
+
+
+
+    }
+
+
+
+
+});
